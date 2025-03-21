3a3697c82079957ea6b685e444cc053ca19afb69
kolbyblack <104000919+kolbyblack@users.noreply.github.com>
2025-03-21 15:09:53 +0000

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -110,7 +110,7 @@ const ContactPage = () => {
                     <Phone className="w-5 h-5 mr-3 mt-1 text-primary" />
                     <div>
                       <h3 className="font-semibold">Phone Number</h3>
-                      <p className="text-foreground/70">+233 12 345 6789</p>
+                      <p className="text-foreground/70">054 123 7516</p>
                     </div>
                   </div>
                   <div className="flex items-start">
